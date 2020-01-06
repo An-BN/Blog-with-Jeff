89af52753b15a206c9f75309a8622ae9ede4cba6
An <anbn247@gmail.com>
2020-01-06 23:04:48 +0700

    Authorization with policies

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-8 col-offset-2">
-                @foreach ($threads as $thread)
+                @forelse ($threads as $thread)
                     <div class="card">
                         <div class="card-header">
                             <div class="level">
@@ -22,7 +22,9 @@
                             <div class="body">{{ $thread->body }}</div>
                         </div>
                     </div>
-                @endforeach
+                @empty
+                    <p>There are no relevant results at this time.</p>
+                @endforelse
             </div>
         </div>
     </div>
